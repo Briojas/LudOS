3bfddad41840c3054742f8f360e15d1ab7abcfa4
Briojas <blake.a.riojas@gmail.com>
2025-10-10 08:48:08 -0500

    fix: replace xorg-x11-apps with xorg-x11-utils package dependency

--- build_files/build.sh
+++ build_files/build.sh
@@ -85,7 +85,7 @@ dnf5 install -y \
     mesa-vulkan-drivers \
     xorg-x11-server-Xwayland \
     openbox \
-    xorg-x11-apps
+    xorg-x11-utils
 
 ### Enable RPM Fusion repositories (required for Steam, NVIDIA drivers, etc.)
 echo "Enabling RPM Fusion repositories..."
